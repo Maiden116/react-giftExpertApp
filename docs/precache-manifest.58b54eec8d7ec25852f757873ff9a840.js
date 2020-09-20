0d75436ffca4964e74ec36023ce76859829d4ed9
Emmanuel Leiva <emma_lthe@hotmail.com>
2020-09-20 18:10:37 -0300

    Se agrega docs

---
+++ docs/precache-manifest.58b54eec8d7ec25852f757873ff9a840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da1c6edcd75cf5cab7f1278a71f2ea6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5134becaee9965b87ba5",
+    "url": "/static/css/main.1a479b09.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5134becaee9965b87ba5",
+    "url": "/static/js/main.1abd7444.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
